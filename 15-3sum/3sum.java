5595471d391fd60e17cd58f6d5e21c37c4a3e895
Aanuj Jain <55945046+aanuj19@users.noreply.github.com>
2026-07-10 09:15:13 +0530

    Time: 29 ms (93.76%) | Memory: 59 MB (74.01%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -3,17 +3,22 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
         for(int i=0; i<nums.length; i++){
-            if(i>0 && nums[i] == nums[i-1]) continue;
+            // fixing the i at first element
+            if(i>0 && nums[i] == nums[i-1]) continue;// optimizing for duplicate values
+            // j will be increasing
             int j=i+1;
+            // k will be decreasing
             int k=nums.length-1;
             while(j<k){
                 int total = nums[i]+nums[j]+nums[k];
+                // if the total is greater than reduce k
                 if(total > 0) k--;
+                // if the total is smaller than increase j
                 else if (total < 0) j++;
                 else{
                     res.add(List.of(nums[i],nums[j],nums[k]));
                     j++;
-                    while(nums[j] == nums[j-1] && j<k) j++;
+                    while(nums[j] == nums[j-1] && j<k) j++; //optimizing for duplicate values
                 }
             }
         }
