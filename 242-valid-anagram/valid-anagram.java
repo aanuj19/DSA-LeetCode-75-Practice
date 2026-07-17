4b49411c2ebfb93c427b718ea04f482b37ffbc5a
Aanuj Jain <55945046+aanuj19@users.noreply.github.com>
2026-07-17 07:28:18 +0530

    Time: 20 ms (8.99%) | Memory: 47.1 MB (5.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,17 +1,30 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
+        if(s.length() != t.length()) return false;
+        HashMap<Character,Integer> sMap = new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            if(sMap.containsKey(s.charAt(i))){
+                int freq = sMap.get(s.charAt(i));
+                sMap.put(s.charAt(i), freq +1);
+            }else{
+                sMap.put(s.charAt(i), 1);
+            }
         }
-        char[] a = s.toCharArray();
-        char[] b = t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-
-        for(int i=0; i<a.length; i++){
-            if(a[i] != b[i]) return false;
+        HashMap<Character,Integer> tMap = new HashMap<>();
+        for(int i=0; i<t.length(); i++){
+            if(tMap.containsKey(t.charAt(i))){
+                int freq = tMap.get(t.charAt(i));
+                tMap.put(t.charAt(i), freq +1);
+            }else{
+                tMap.put(t.charAt(i), 1);
+            }
+        }
+        for(char key : sMap.keySet()){
+            int val1 = sMap.get(key);
+            if(!tMap.containsKey(key)) return false;
+            int val2 = tMap.get(key);
+            if (val1 != val2) return false;
         }
         return true;
-
     }
 }
